cf6d9ec73326d2f79d99785b89826e7136a63c43
FAIZABIDIN12 <faizabidin72@gmail.com>
2024-05-17 15:14:00 +0700

    perbaiki lagi

--- app/Controllers/KasController.php
+++ app/Controllers/KasController.php
@@ -25,24 +25,24 @@ class KasController extends BaseController
         // Memproses data pendapatan berdasarkan checkin
         $pendapatan = [];
         foreach ($checkinData as $checkin) {
+            // Ini akan membuat pendapatan dari setiap data checkin
             $total_hari = (new \DateTime($checkin['checkout_plan']))->diff(new \DateTime($checkin['checkin']))->days;
             $rate_per_hari = $checkin['rate'] / $total_hari; // Asumsi bahwa rate adalah total biaya menginap
             $bayar = $checkin['bayar'];
             $remaining_bayar = $bayar;
 
-            // Memasukkan data pembayaran ke dalam pendapatan
             while ($remaining_bayar > 0 && $total_hari > 0) {
                 if ($remaining_bayar >= $rate_per_hari) {
                     $pendapatan[] = [
                         'tgl_transaksi' => $checkin['checkin'],
-                        'uraian' => 'Reservasi ' . $checkin['nama'],
+                        'uraian' => 'Check-in ' . $checkin['nama'], // Ubah dari 'Reservasi' menjadi 'Check-in'
                         'lunas' => $rate_per_hari
                     ];
                     $remaining_bayar -= $rate_per_hari;
                 } else {
                     $pendapatan[] = [
                         'tgl_transaksi' => $checkin['checkin'],
-                        'uraian' => 'Reservasi ' . $checkin['nama'],
+                        'uraian' => 'Check-in ' . $checkin['nama'], // Ubah dari 'Reservasi' menjadi 'Check-in'
                         'lunas' => $remaining_bayar
                     ];
                     $remaining_bayar = 0;
@@ -53,12 +53,12 @@ class KasController extends BaseController
 
         // Memproses data pendapatan berdasarkan reservasi
         foreach ($reservationData as $reservation) {
+            // Saya asumsikan ini seharusnya berdasarkan data reservasi
             $total_hari = (new \DateTime($reservation['tgl_checkout']))->diff(new \DateTime($reservation['tgl_checkin']))->days;
             $rate_per_hari = $reservation['rate'] / $total_hari;
             $bayar = $reservation['bayar'];
             $remaining_bayar = $bayar;
 
-            // Memasukkan data pembayaran ke dalam pendapatan
             while ($remaining_bayar > 0 && $total_hari > 0) {
                 if ($remaining_bayar >= $rate_per_hari) {
                     $pendapatan[] = [
